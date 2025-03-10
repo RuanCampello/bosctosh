b3dcf399af468ed2120787c9eadc05b139ebd439
RuanCampello <ruancampello888@gmail.com>
2025-03-10 18:16:05 -0300

    feat: add chicago font

--- src/app/components/header.tsx
+++ src/app/components/header.tsx
@@ -1,6 +1,6 @@
 export default function Header() {
   return (
-    <header className='border-b-4 text-background border-background py-2 px-4 bg-foreground rounded-t-2xl'>
+    <header className='font-chicago border-b-4 text-background border-background py-2 px-4 bg-foreground rounded-t-2xl'>
       pear
     </header>
   );
