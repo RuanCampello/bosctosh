921020c7664888db00f2b0e5284d925695c2323a
RuanCampello <ruancampello888@gmail.com>
2025-03-10 20:29:15 -0300

    feat: add profile and date

--- src/app/components/header.tsx
+++ src/app/components/header.tsx
@@ -1,7 +1,34 @@
+'use client';
+
+import Profile from '@/assets/profile.png';
+import Image from 'next/image';
+import { useEffect, useState } from 'react';
+
 export default function Header() {
+  const [date, setDate] = useState(new Date());
+
+  useEffect(() => {
+    const intervalId = setInterval(() => {
+      setDate(new Date());
+    }, 500);
+
+    return () => clearInterval(intervalId);
+  }, []);
+
+  const dateString = date.toLocaleDateString('en-GB');
+  const time = date.toLocaleTimeString('en');
+
   return (
-    <header className='font-chicago border-b-4 text-background border-background py-2 px-4 bg-foreground rounded-t-2xl'>
-      pear
+    <header className='font-chicago flex justify-between items-center border-b-4 text-background border-background py-2.5 px-4 bg-foreground rounded-t-2xl'>
+      <div>pear</div>
+      <div className='flex items-center gap-4'>
+        <Image src={Profile} alt='Logo' className='w-8 h-8' />
+
+        <div className='flex flex-col text-sm'>
+          <span suppressHydrationWarning>{time}</span>
+          <span suppressHydrationWarning>{dateString}</span>
+        </div>
+      </div>
     </header>
   );
 }
